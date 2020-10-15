b839f559e2b02b991bfdc86aaba5877e091726ab
sg002-reactive-prog <44365987+sg002-reactive-prog@users.noreply.github.com>
2020-10-15 10:47:40 +0100

    Update Demo.java
    Testing pipeline

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -3,7 +3,7 @@ package com.microsoft.demo;
 public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
-            System.out.println("I am covered! Change me");
+            System.out.println("I am covered! Need to be in lowercase");
             return;
         }
 
